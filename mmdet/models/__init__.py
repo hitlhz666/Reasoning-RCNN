3d38b97d9ebb7be108a7236e6eda98a0ddfa0f5d
刘恒正 <68774608+hitlhz666@users.noreply.github.com>
2020-11-27 14:57:50 +0800

    Update __init__.py

--- mmdet/models/__init__.py
+++ mmdet/models/__init__.py
@@ -1,1 +1,11 @@
+from .anchor_heads import *
+from .backbones import *
+from .bbox_heads import *
+from .detectors import *
+from .mask_heads import *
+from .necks import *
+from .roi_extractors import *
+from .utils import *
+
+
 
