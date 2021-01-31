a96210173bb617a8f227f92afac592d6023ce3f7
刘恒正 <68774608+hitlhz666@users.noreply.github.com>
2021-01-31 14:22:07 +0800

    Update __init__.py

--- mmdet/core/anchor/__init__.py
+++ mmdet/core/anchor/__init__.py
@@ -1,6 +1,8 @@
-# 在pytorch中， AnchorGenerator主要用于生成候选框，该类存储在torchvision/models/detection/rpn.py中
-# 用generate_anchors产生多种坐标变换，这种坐标变换由scale和ratio来，相当于提前计算好。
-# anchor_target_layer先计算的是从feature map映射到原图的中点坐标，然后根据多种坐标变换生成不同的框。
+# 在pytorch中, AnchorGenerator 主要用于生成候选框，该类存储在torchvision/models/detection/rpn.py中
+# 用 generate_anchors 产生多种坐标变换，这种坐标变换由scale和ratio来，相当于提前计算好。
+# anchor_target_layer 先计算的是从feature map映射到原图的中点坐标，然后根据多种坐标变换生成不同的框。
+
+# anchor 锚, 锚点生成
 
 anchor_target_layer层是产生在rpn训练阶段产生anchors的层
 
