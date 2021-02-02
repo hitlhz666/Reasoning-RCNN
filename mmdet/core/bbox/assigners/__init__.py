c2db35b2d7c880236c26837271a9139b2e5446e6
刘恒正 <68774608+hitlhz666@users.noreply.github.com>
2021-02-02 21:44:07 +0800

    Update __init__.py

--- mmdet/core/bbox/assigners/__init__.py
+++ mmdet/core/bbox/assigners/__init__.py
@@ -2,4 +2,11 @@ from .base_assigner import BaseAssigner
 from .max_iou_assigner import MaxIoUAssigner
 from .assign_result import AssignResult
 
+"""
+
+assign和sample是在anchor target中的核心操作
+assign一般基于IOU, mmdet中也有基于atss和基于point的等
+sample一般为随机, 也有ohem的, 基于伪标签的
+"""
+
 __all__ = ['BaseAssigner', 'MaxIoUAssigner', 'AssignResult']
