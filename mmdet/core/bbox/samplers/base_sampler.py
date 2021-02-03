a71ab49360a0ce5fbf601393b34428b47887f3ea
刘恒正 <68774608+hitlhz666@users.noreply.github.com>
2021-02-03 16:14:22 +0800

    Update base_sampler.py

--- mmdet/core/bbox/samplers/base_sampler.py
+++ mmdet/core/bbox/samplers/base_sampler.py
@@ -20,6 +20,9 @@ class BaseSampler(metaclass=ABCMeta):
         self.pos_sampler = self
         self.neg_sampler = self
 
+        
+    # 多态性是指具有不同功能的函数可以使用相同的函数名，这样就可以用一个函数名调用不同内容的函数    
+    # 含有@abstractmethod修饰的父类不能实例化，但是继承的子类必须实现@abstractmethod装饰的方法    
     @abstractmethod
     def _sample_pos(self, assign_result, num_expected, **kwargs):
         pass
@@ -34,9 +37,9 @@ class BaseSampler(metaclass=ABCMeta):
                gt_bboxes,
                gt_labels=None,
                **kwargs):
-        """Sample positive and negative bboxes.
+        """Sample positive and negative bboxes.    取样正负bboxes
 
-        This is a simple implementation of bbox sampling given candidates,
+        This is a simple implementation of bbox sampling given candidates,       这是一个简单的bbox采样实现, 分配结果和ground truth bboxes
         assigning results and ground truth bboxes.
 
         Args:
@@ -46,7 +49,20 @@ class BaseSampler(metaclass=ABCMeta):
             gt_labels (Tensor, optional): Class labels of ground truth bboxes.
 
         Returns:
-            :obj:`SamplingResult`: Sampling result.
+            :obj:`SamplingResult`: Sampling result.      采样结果
+        Example:
+            >>> from mmdet.core.bbox import RandomSampler
+            >>> from mmdet.core.bbox import AssignResult
+            >>> from mmdet.core.bbox.demodata import ensure_rng, random_boxes
+            >>> rng = ensure_rng(None)
+            >>> assign_result = AssignResult.random(rng=rng)
+            >>> bboxes = random_boxes(assign_result.num_preds, rng=rng)
+            >>> gt_bboxes = random_boxes(assign_result.num_gts, rng=rng)
+            >>> gt_labels = None
+            >>> self = RandomSampler(num=32, pos_fraction=0.5, neg_pos_ub=-1,
+            >>>                      add_gt_as_proposals=False)
+            >>> self = self.sample(assign_result, bboxes, gt_bboxes, gt_labels)
+             
         """
         bboxes = bboxes[:, :4]
 
@@ -57,21 +73,21 @@ class BaseSampler(metaclass=ABCMeta):
             gt_ones = bboxes.new_ones(gt_bboxes.shape[0], dtype=torch.uint8)
             gt_flags = torch.cat([gt_ones, gt_flags])
 
-        num_expected_pos = int(self.num * self.pos_fraction)
+        num_expected_pos = int(self.num * self.pos_fraction)     # 计算出选择的正样本的个数
         pos_inds = self.pos_sampler._sample_pos(
-            assign_result, num_expected_pos, bboxes=bboxes, **kwargs)
-        # We found that sampled indices have duplicated items occasionally.
+            assign_result, num_expected_pos, bboxes=bboxes, **kwargs)      #从所有正样本中随机选择出num_expected_pos 个正样本
+        # We found that sampled indices have duplicated items occasionally.    我们发现抽样的索引偶尔会有重复的项
         # (may be a bug of PyTorch)
-        pos_inds = pos_inds.unique()
-        num_sampled_pos = pos_inds.numel()
-        num_expected_neg = self.num - num_sampled_pos
+        pos_inds = pos_inds.unique()                      # unique()：返回参数数组中所有不同的值, 并按照从小到大排序可选参数
+        num_sampled_pos = pos_inds.numel()                # numel() 返回一个tensor变量内所有元素个数, 可以理解为矩阵内元素的个数
+        num_expected_neg = self.num - num_sampled_pos     # 负样本等于总共需要的样本数减去已经选择正样本数目
         if self.neg_pos_ub >= 0:
             _pos = max(1, num_sampled_pos)
             neg_upper_bound = int(self.neg_pos_ub * _pos)
             if num_expected_neg > neg_upper_bound:
                 num_expected_neg = neg_upper_bound
         neg_inds = self.neg_sampler._sample_neg(
-            assign_result, num_expected_neg, bboxes=bboxes, **kwargs)
+            assign_result, num_expected_neg, bboxes=bboxes, **kwargs)     #从所有负样本中随机选择num_expected_neg个负样本
         neg_inds = neg_inds.unique()
 
         return SamplingResult(pos_inds, neg_inds, bboxes, gt_bboxes,
