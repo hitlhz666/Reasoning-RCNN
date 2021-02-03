d2599dc82cba55fc40beb2107ed3f27b70d3098e
刘恒正 <68774608+hitlhz666@users.noreply.github.com>
2021-02-03 16:21:09 +0800

    Update combined_sampler.py

--- mmdet/core/bbox/samplers/combined_sampler.py
+++ mmdet/core/bbox/samplers/combined_sampler.py
@@ -5,12 +5,12 @@ from ..assign_sampling import build_sampler
 class CombinedSampler(BaseSampler):
 
     def __init__(self, pos_sampler, neg_sampler, **kwargs):
-        super(CombinedSampler, self).__init__(**kwargs)
-        self.pos_sampler = build_sampler(pos_sampler, **kwargs)
+        super(CombinedSampler, self).__init__(**kwargs)               # super(父类,self).__init__() 可以指定调用的父类
+        self.pos_sampler = build_sampler(pos_sampler, **kwargs)       # **kwargs用作传递键值可变长参数列表(比如字典)
         self.neg_sampler = build_sampler(neg_sampler, **kwargs)
 
     def _sample_pos(self, **kwargs):
-        raise NotImplementedError
+        raise NotImplementedError              # 子类没有实现父类要求一定要实现的接口
 
     def _sample_neg(self, **kwargs):
         raise NotImplementedError
