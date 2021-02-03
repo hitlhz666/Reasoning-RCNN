76ac37add688b2c06d56636be90b3c946d960cc0
刘恒正 <68774608+hitlhz666@users.noreply.github.com>
2021-02-03 18:09:23 +0800

    Update instance_balanced_pos_sampler.py

--- mmdet/core/bbox/samplers/instance_balanced_pos_sampler.py
+++ mmdet/core/bbox/samplers/instance_balanced_pos_sampler.py
@@ -3,16 +3,22 @@ import torch
 
 from .random_sampler import RandomSampler
 
+"""
+字面意思就是每个instance抽样出来的样本要均衡，不能cat抽个1000个，dog只抽个10个
+改写了RandomSampler里的sample_pos函数，同样也是改了最后那个pos_index大于expected的那部分
 
+"""
 class InstanceBalancedPosSampler(RandomSampler):
 
     def _sample_pos(self, assign_result, num_expected, **kwargs):
-        pos_inds = torch.nonzero(assign_result.gt_inds > 0)
-        if pos_inds.numel() != 0:
+        pos_inds = torch.nonzero(assign_result.gt_inds > 0)      # 首先看一下给的bboxes里面有哪些label是大于0的, 得到了他们的index
+        
+        if pos_inds.numel() != 0:                   # 首先只要这个index的数目不是0个, 这些就都可以是positive sample
             pos_inds = pos_inds.squeeze(1)
-        if pos_inds.numel() <= num_expected:
+        
+        if pos_inds.numel() <= num_expected:        # 当pos_indxs的数目小于想要的sample的数目的时候, 就直接用这个pos_index
             return pos_inds
-        else:
+        else:                                       # 反之就从这么多index里 用某种方法(区别在这里: 随机/类均衡法/OHEM法) 采样num_expected个出来
             unique_gt_inds = assign_result.gt_inds[pos_inds].unique()
             num_gts = len(unique_gt_inds)
             num_per_gt = int(round(num_expected / float(num_gts)) + 1)
